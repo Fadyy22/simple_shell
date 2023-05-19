dec0bf0d7521512d8f1a9ea4dc2a47ddd037a55d
abdullah2422 <abdullah@gmail.com>
2023-05-19 10:28:56 -0700

    shell

---
+++ _stringlen.c
@@ -0,0 +1,17 @@
+#include "shell.h"
+
+/**
+ * _stringlen - for loop input the user
+ * @input: stringlen by the user
+ * Return: stringlen
+ */
+
+int _stringlen(char *input)
+{
+
+	int stringlen;
+
+	for (stringlen = 0; input[stringlen] != '\0' stringlen++)
+
+		return (stringlen);
+}
